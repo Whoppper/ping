97612538b1468af82290930ec3cbc9ad3eae6779
alegay <axel.legay@davidson.fr>
2020-10-13 21:38:17 +0200

    c1

--- ft_ping/main.cpp
+++ ft_ping/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-    cout << "toto" << endl;
-
+    if (ac != 2)
+    {
+        printf("usage: %s IP address\n", av[0]);
+        return 1;
+    }
     return 0;
 }
