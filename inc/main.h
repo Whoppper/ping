9c1b67cf8eb8110355ce23f926dd201ed20ef478
scaussin <scaussin@student.42.fr>
2020-10-22 21:43:25 +0200

    cleanning repo

--- inc/main.h
+++ inc/main.h
@@ -5,6 +5,29 @@
 #ifndef PING_MAIN_H
 #define PING_MAIN_H
 
-void printAddrInfo(addrinfo *pAddrInfo);
+#include <iostream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <iomanip>
+#include <time.h>
+#include <signal.h>
+#include <sys/time.h>
+
+uint16_t	icmpChecksum(uint16_t *data, uint32_t len);
+void        hexdumpBuf(char *buf, uint32_t len);
+double      getDiffTimeval(const timeval &t1, const timeval &t2);
+bool        isEchoReply(uint8_t *buf, ssize_t retRecv);
+
+using std::cout;
+using std::endl;
+
 
 #endif //PING_MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,4 @@
-#include <iostream>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <iomanip>
-#include <time.h>
-#include <signal.h>
-#include <sys/time.h>
-
-using std::cout;
-using std::endl;
-
-uint16_t	icmpChecksum(uint16_t *data, uint32_t len);
-void        hexdumpBuf(char *buf, uint32_t len);
-double      getDiffTimeval(const timeval &t1, const timeval &t2);
-bool        isEchoReply(uint8_t *buf, ssize_t retRecv);
+#include "main.h"
 
 bool loop = true;
 bool isTimeout = false;
